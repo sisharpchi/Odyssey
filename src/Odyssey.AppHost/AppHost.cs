6e285a79acddb6afa47e29e5e4c4a8e12648aed3
Ahmadjon Qudratov <qahmadjon11@gmail.com>
2026-03-13 11:54:37 +0500

    feat: the project is fully set up and the architecture is up

---
+++ src/Odyssey.AppHost/AppHost.cs
@@ -0,0 +1,67 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var postgres = builder.AddPostgres("postgres")
+    .WithDataVolume()
+    .WithPgAdmin();
+
+var redis = builder.AddRedis("cache")
+    .WithDataVolume();
+
+var authDb = postgres.AddDatabase("odyssey_auth_db");
+var homestayDb = postgres.AddDatabase("odyssey_homestay_db");
+var tripDb = postgres.AddDatabase("odyssey_trip_db");
+var notifyDb = postgres.AddDatabase("odyssey_notify_db");
+
+#region need
+//var apiService = builder.AddProject<Projects.Odyssey_ApiService>("apiservice")
+//    .WithHttpHealthCheck("/health");
+
+//builder.AddProject<Projects.Odyssey_Web>("webfrontend")
+//    .WithExternalHttpEndpoints()
+//    .WithHttpHealthCheck("/health")
+//    .WithReference(cache)
+//    .WaitFor(cache)
+//    .WithReference(apiService)
+//    .WaitFor(apiService);
+#endregion
+
+var authApi = builder.AddProject<Projects.AuthService_API>("authservice-api")
+    .WithReference(authDb)
+    .WaitFor(authDb)
+    .WithReference(redis)
+    .WaitFor(redis)
+    .WithHttpHealthCheck("/health");
+
+var homestayApi = builder.AddProject<Projects.HomestayService_API>("homestayservice-api")
+    .WithReference(homestayDb)
+    .WaitFor(homestayDb)
+    .WithReference(redis)
+    .WaitFor(redis)
+    .WithReference(authApi)
+    .WaitFor(authApi)
+    .WithHttpHealthCheck("/health");
+
+var tripApi = builder.AddProject<Projects.TripService_API>("tripservice-api")
+    .WithReference(tripDb)
+    .WaitFor(tripDb)
+    .WithReference(redis)
+    .WaitFor(redis)
+    .WithReference(authApi)
+    .WaitFor(authApi)
+    .WithHttpHealthCheck("/health");
+
+var aiGuideApi = builder.AddProject<Projects.AIGuideService_API>("aiguideservice-api")
+    .WithReference(redis)
+    .WaitFor(redis)
+    .WithReference(authApi)
+    .WaitFor(authApi)
+    .WithHttpHealthCheck("/health");
+
+var notifyApi = builder.AddProject<Projects.NotificationService_API>("notificationservice-api")
+    .WithReference(notifyDb)
+    .WaitFor(notifyDb)
+    .WithReference(redis)
+    .WaitFor(redis)
+    .WithHttpHealthCheck("/health");
+
+builder.Build().Run();
